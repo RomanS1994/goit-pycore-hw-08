00cf1bd5c0906944254c6c1dfda53c28142a4476
Roman Stryzhka <rstrizhko1994@gmail.com>
2024-10-31 16:13:03 +0100

    added dokumentations

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# goit-pycore-hw-08
+This code implements an assistant for managing contact information and birthday reminders.
+
+Classes:
+
+- `Field` — base class for creating contact fields, including `Name`, `Phone`, and `Birthday`.
+- `Name` — subclass of `Field` representing a contact's name.
+- `Phone` — subclass of `Field` representing a phone number, with format validation.
+- `Birthday` — subclass of `Field` storing a birthday date in the `DD.MM.YYYY` format.
+- `Record` — stores contact information (name, phones, birthday) and allows phone editing.
+- `AddressBook` — a dictionary of contacts with support for adding, deleting, searching, and retrieving birthday reminders.
+
+Decorator:
+
+- `input_error` — handles exceptions raised during command execution.
+
+Main functions:
+
+- `parse_input` — parses the user input into a command and arguments.
+- `add_contact`, `change_number`, `show_phones`, `show_all`, `add_birthday`, `show_birthday`, `show_reminder` — user command handlers.
+- `save_data`, `load_data` — functions for serializing and deserializing the address book data.
--- assistant_bot.py
+++ assistant_bot.py
@@ -1,8 +1,30 @@
+""""
+This code implements an assistant for managing contact information and birthday reminders.
+
+Classes:
+- `Field` — base class for creating contact fields, including `Name`, `Phone`, and `Birthday`.
+- `Name` — subclass of `Field` representing a contact's name.
+- `Phone` — subclass of `Field` representing a phone number, with format validation.
+- `Birthday` — subclass of `Field` storing a birthday date in the `DD.MM.YYYY` format.
+- `Record` — stores contact information (name, phones, birthday) and allows phone editing.
+- `AddressBook` — a dictionary of contacts with support for adding, deleting, searching, and retrieving birthday reminders.
+
+Decorator:
+- `input_error` — handles exceptions raised during command execution.
+
+Main functions:
+- `parse_input` — parses the user input into a command and arguments.
+- `add_contact`, `change_number`, `show_phones`, `show_all`, `add_birthday`, `show_birthday`, `show_reminder` — user command handlers.
+- `save_data`, `load_data` — functions for serializing and deserializing the address book data.
+
+"""
 
 from collections import UserDict
 from datetime import datetime, date, timedelta
 import re
 import pickle
+
+
 
 class Field:
     def __init__(self, value):
@@ -117,9 +139,12 @@ class AddressBook(UserDict):
 
 
     def get_upcoming_birthdays(self):
+        """
+        Returns a list of contacts with birthdays in the next 7 days.
+        Returns:
+            list of dict:
+        """
         today = datetime.today().date()
-        # print(f"today is: {today}")
-
         reminder = []
 
         for contact in self.data.values():
